c315803eaf4236fe0c09ba97f65b4e832e020029
SivaChandra422 <siva.sivachandra23@gmail.com>
2025-05-18 10:16:02 +0530

    Add Apk forge module

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from src.routes.github_push import router as github_router
 from src.routes.build_trigger import router as build_router
 from src.routes.download_status import router as download_router
 from src.routes.seed_core import router as seed_router
+from src.routes.apk_forge import router as forge_router
+
 
 app = FastAPI()
 
@@ -14,6 +16,7 @@ app.include_router(github_router)
 app.include_router(build_router)
 app.include_router(download_router)
 app.include_router(seed_router)
+app.include_router(forge_router)
 
 @app.get("/")
 def read_root():
